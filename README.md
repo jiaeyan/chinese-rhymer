5aea6094ff4b49ea4846e028a90bd4283818b0f1
jiaeyan <svenyan234@gmail.com>
2018-05-11 13:05:40 -0400

    updated version

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # 中文押韵器 Chinese Rhyme
 ![Build](https://img.shields.io/badge/build-passing-green.svg)
-[![PyPI](https://img.shields.io/badge/pypi-v0.2.4.2-blue.svg)](https://pypi.org/project/chrhyme/)
+[![PyPI](https://img.shields.io/badge/pypi-v0.2.5-blue.svg)](https://pypi.org/project/chrhyme/)
 ![License](https://img.shields.io/badge/license-MIT-yellow.svg)
 
 ## 一、简介
 这是一款帮助 _诗歌爱好者_ 和 _说唱爱好者_ 寻找押韵灵感的小工具。  
 只要输入一个目标词，和一些限制条件，该工具就能查询词库中所有满足条件的押韵词。  
-目前提供 __单押__， __双押__，__三押__ 和 __四押__ 查询功能， 同时支持俗语、科技术语等 __词长扩展押韵__。  
+目前提供 __单押__， __双押__，__三押__ 和 __四押__ 查询功能， 同时支持 _俗语_、_科技术语_ 等 __词长扩展押韵__。  
 
 ## 二、安装
-系统要求：Python 3 以上。  
+系统要求：Python 2 或 3。  
 
     $ pip install chrhyme
 
@@ -41,12 +41,12 @@
 
 
 ## 四、版本更新
-* __v0.2.4.2__ (05/11/2018)  
+* __v0.2.5__ (05/11/2018)  
 1、开放单押功能  
-2、支持扩展押韵，如输入词为"报恩"，可以查询到"一朝天子一朝臣"，即长度扩展的匹配词尾部与输入词押韵。  
-3、区分 si (思), shi (诗), ji (机) 三类不同发音的 i  
+2、支持扩展押韵，如输入词为`报恩`，可以查询到`一朝天子一朝臣`，即长度扩展的匹配词尾部与输入词押韵。  
+3、区分 __si__ `(思)`, __shi__ `(诗)`, __ji__ `(机)` 三类不同发音的 __i__  
 4、扩大词库 (_70万细胞词_)  
-5、兼容所有Python 3 版本
+5、兼容Python 2 和 3 所有版本
 
 * __v0.1.5__ (05/04/2018)  
 1、区分发音不同的 an 和 ian ，以及 e 和 ie 
--- setup.py
+++ setup.py
@@ -6,19 +6,22 @@ from setuptools import setup, find_packages
 
 
 requirements = ["pypinyin"]
+if sys.version_info[:2] < (2, 7):
+    requirements.append('argparse')
 if sys.version_info[:2] < (3, 4):
     requirements.append('enum34')
 if sys.version_info[:2] < (3, 5):
     requirements.append('typing')
 
 extras_require = {
+    ':python_version<"2.7"': ['argparse'],
     ':python_version<"3.4"': ['enum34'],
     ':python_version<"3.5"': ['typing'],
 }
 
 setup(
     name="chrhyme",
-    version="0.2.4.2",
+    version="0.2.5",
     author="Jiajie Yan",
     author_email="jiaeyan@gmail.com",
     description="Find rhymes for Chinese words.",
@@ -30,7 +33,7 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     extras_require=extras_require,
-    python_requires='>=3',
+    python_requires='>=2.6, >=3',
     include_package_data=True,
     # package_data={
     #     'chrhyme': ['data/phrase_dict.txt', 'data/demo.png'],
@@ -40,8 +43,10 @@ setup(
         'console_scripts': ['chrhyme = chrhyme.__main__:main']
     },
     classifiers=[
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
